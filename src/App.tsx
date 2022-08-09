8f59f2be9c5c88f46fbbae519daf8ec78eb89afd
MaxBQ <maksat.bek93@gmail.com>
2022-08-09 11:21:18 +0500

    fix: time

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./index.css";
+import {
+	AppContainer,
+	Clock,
+	Hours,
+	Minutes,
+	Seconds,
+	Semicircle,
+} from "./styled";
+import { IClockProps } from "./styled";
+
+export const App = () => {
+	const [time, setTime] = useState<IClockProps>({
+		hour: 0,
+		minute: 0,
+		seconds: 0,
+	});
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (time.hour >= 24) {
+				setTime({ ...time, hour: 1 });
+			}
+			if (time.minute >= 60) {
+				setTime({
+					...time,
+					minute: 1,
+					hour: time.hour + 1,
+				});
+			}
+			if (time.seconds >= 60) {
+				setTime({
+					...time,
+					seconds: 1,
+					minute: time.minute + 1,
+				});
+			}
+			if (time.seconds < 60) {
+				setTime({ ...time, seconds: time.seconds + 1 });
+			}
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, [time]);
+
+	useEffect(() => {
+		axios
+			.get("/api/Time/current/zone", {
+				params: {
+					timeZone: "Asia/Ashgabat",
+				},
+			})
+			.then((response) => {
+				const { hour, minute, seconds } = response.data;
+				setTime({ hour, minute, seconds });
+			});
+	}, []);
+
+	return (
+		<AppContainer>
+			<Clock>
+				<Hours time={time} />
+				<Minutes time={time} />
+				<Seconds time={time} />
+			</Clock>
+			<Semicircle />
+		</AppContainer>
+	);
+};
 
 export default App;
---
+++ src/styled.ts
@@ -0,0 +1,73 @@
+import styled from "styled-components"
+
+export interface IClockProps {
+  hour: number;
+  minute: number;
+  seconds: number;
+}
+
+interface IDataProps {
+  time: IClockProps
+}
+
+export const AppContainer = styled.div`
+display: flex;
+align-items: flex-start;
+flex-direction: column;
+background-color: black;
+height: 100%;
+width: 100%;
+padding: 20px;
+`
+
+export const Clock = styled.div`
+background-color: white;
+border-radius: 50%;
+border: red 1px solid;
+height: 20rem;
+width: 20rem;
+margin-bottom:0.5rem;
+position: relative;
+margin-left: 42%;
+margin-right: 42%;
+`
+
+
+export const Hours = styled.div<IDataProps>`
+  background-color: black;
+  border-radius: 2.5px;
+  height: 130px;
+  left: calc(50% - 2.5px);
+  position: absolute;
+  top: 35px;
+  width: 5px;
+  transform-origin: center calc(100% - 5px);
+  transform: rotateZ(${({ time }) => (time.hour % 12) * 30}deg);
+
+`
+export const Minutes = styled(Hours)`
+  height: 145px;
+  top: 20px;
+
+  transform: rotateZ(${({ time }) => (time.minute * 60 + time.seconds) * 0.1}deg);
+`
+
+export const Seconds = styled(Hours)`
+  background-color: red;
+  height: 150px;
+  top: 15px;
+  width: 1px;
+  transform: rotateZ(${({ time }) => time.seconds * 6}deg);
+
+`
+
+export const Semicircle = styled.div`
+width: 20rem;
+  height: 10rem;
+  background-color: blueviolet;
+  border-radius: 10rem 10rem 0 0;
+  position: relative;
+  margin-left: 42%;
+  margin-right: 42%;
+  margin-top: 25px;
+`
